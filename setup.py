e1667f94c9406e1e7995dba1d82b557e640dc11b
Dan Halbert <halbert@halwitz.org>
2021-03-25 09:31:48 -0400

    put bleak requirement in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     # Author details
     author="Adafruit Industries",
     author_email="circuitpython@adafruit.com",
-    install_requires=["bleak"],
+    install_requires=["adafruit-blinka", "bleak==0.11.0"],
     # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
